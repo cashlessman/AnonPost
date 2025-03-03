dfdaed47afb0aca3925b73ff792793d3c904ebd5
cashlessman <cashlessman@proton.me>
2025-03-04 00:02:41 +0530

    update domain manifest

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -3,9 +3,9 @@ export async function GET() {
 
   const config = {
     accountAssociation: {
-      header: "",
-      payload: "",
-      signature:""
+      header: "eyJmaWQiOjI2ODQzOCwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDIxODA4RUUzMjBlREY2NGMwMTlBNmJiMEY3RTRiRkIzZDYyRjA2RWMifQ",
+      payload: "eyJkb21haW4iOiJhbm9uLXBvc3QudmVyY2VsLmFwcCJ9",
+      signature:"MHg0ZDk4N2FiNjkzMzBiMmIwNTQyYTUxZTllYzAxNTM1YWNjNTAzNjE5MDU2ZjIzOTQ4MzYxMDNkMmRjNjEwYWQ5NjI2ZmVlNGMwY2Y2YmExMTZiNDdjZjk1MmI4NjVkOTMyMTliMTM2ODUwMTk2MDc1NDYyMGM0MjMwODIxOWUyNTFi"
     },
     frame: {
       version: "1",
